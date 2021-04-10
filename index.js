9ba87667a53841025c01bf2f62a845f801baa40f
omniboyok <barrea.pablo@gmail.com>
2021-04-10 14:13:05 -0300

    new cron messages

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const _ = require("lodash");
 let data = require("./messages.json");
 const consejos = data.mensajes;
 const TelegramBot = require("node-telegram-bot-api");
+var CronJob = require("cron").CronJob;
+
+const SILENT_MESSAGE = {
+  parse_mode: "HTML",
+  disable_notification: true,
+};
 
 // replace the value below with the Telegram token you receive from @BotFather
 const token = process.env.BOT_TOKEN;
@@ -43,6 +49,8 @@ bot.on("message", (msg) => {
       // send a recomendation
       bot.sendMessage(chatId, "<code>" + consejo + "</code> \u{1F916}", {
         parse_mode: "HTML",
+        reply_to_message_id: messageId,
+        disable_notification: true,
       });
       ready = true;
     }
@@ -61,3 +69,33 @@ bot.on("message", (msg) => {
 bot.on("webhook_error", (error) => {
   console.log(error.code); // => 'EPARSE'
 });
+
+var job = new CronJob(
+  "30 9 * * 1-5",
+  function () {
+    bot.sendMessage(
+      process.env.MY_GROUP,
+      "<code>Hola, se van a conectar a la daily? </code> \u{1f412}",
+      SILENT_MESSAGE
+    );
+  },
+  null,
+  true,
+  "America/Argentina/Buenos_Aires"
+);
+
+var job = new CronJob(
+  "30 18 * * 1-5",
+  function () {
+    bot.sendMessage(
+      process.env.MY_GROUP,
+      "<code>Hola, no se olviden de cerrar las cards, como vienen con eso? </code> \u{1f412}",
+      SILENT_MESSAGE
+    );
+  },
+  null,
+  true,
+  "America/Argentina/Buenos_Aires"
+);
+
+job.start();
